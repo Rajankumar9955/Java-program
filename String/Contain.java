48d78669d7841e9925d4898d09b0cdd019320c9a
Rajan <krajan92946@gmail.com>
2024-06-05 18:40:22 +0530

    String methods

---
+++ String/Contain.java
@@ -0,0 +1,62 @@
+class Contain{
+public static void main(String args[]){
+
+//contains method() in java programming
+/*
+Syntex:
+String a="abcd";
+boolean b= a.contains("b");
+print (b) //true
+*/
+
+/*
+String str="Wel-come to bhopal";
+Boolean b=str.contains("to");
+System.out.println(b); //true
+b=str.contains("java");
+System.out.println(b);
+*/
+
+
+/*
+it is used to check a string ends with a suffix 
+Syntax:
+boolean endswith();
+*/
+ 
+/*String str="www.cybrom.com";
+boolean b= str.endsWith(".com);
+System.out.println(b);
+b=str.endsWith("www")
+System.out.println(b);*/
+
+
+//startswith() method in java programming
+
+/*String str="www.cybrom.com";
+boolean b=str.startsWith("www");
+system.out.println(b);
+b=str.statsWith(".com");
+System.out.println(b);
+*/
+
+//indexOf() Method in java programming
+
+/*String r="Welcome";
+int n=r.indexOf("W");
+int m=r.indexOf("m",5); // 5 is position of index
+int o=r.indexOf("me");
+System.out.println(n);
+System.out.println(m);
+System.out.println(o);
+*/
+
+//lastIndexOf() method in java programming
+String r="Welcome";
+int n=r.lastIndexOf("o");
+System.out.println(n);
+
+
+
+}
+}
