ac6603390ffff1c3623de5f894fae46e47247f1c
Rajan <krajan92946@gmail.com>
2024-06-06 18:37:35 +0530

    lower upper case

---
+++ String/lenghth.java
@@ -0,0 +1,47 @@
+
+class lenghth{
+public static void main(String args[]){
+
+
+//length method() in java programming
+
+/*String s="Wel come";
+int r=s.length();
+System.out.println("Length of string:"+r);
+*/
+
+
+//Splite() method in java programming
+
+/*String s1="Welcome to bhopal this is city of lake";
+String arr[]=s1.split("");
+int len=arr.length;
+System.out.println("Number of words:"+(len-1));
+*/
+
+//Substring() method in java programming
+
+/*String s1="Welcome";
+String a=s1.substring(3);
+System.out.println("Substring:"+a);
+String b=s1.substring(0,4);
+System.out.println("Starting four element:"+b);
+*/
+
+//toChar array
+
+/*String s1="welcome";
+char arr[]=s1.toCharArray();
+System.out.println("Length of the charater array:"+arr.length);
+*/
+
+
+//toUpperCase() and toLowerCase in java programing
+
+String s1="Welcome";
+String x=s1.toUpperCase();
+String y=s1.toLowerCase();
+System.out.println("LowerCase:"+y);
+System.out.println("Upper case:"+x);
+}
+}
