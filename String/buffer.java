3e356e623cffec8ef42e02cc29733185e6a74e22
Rajan <krajan92946@gmail.com>
2024-06-07 18:34:31 +0530

    Strign Buffer

---
+++ String/buffer.java
@@ -0,0 +1,10 @@
+//string Buffer() it is mutable or isme //modification kr sakte hai
+
+class buffer{
+public static void main(String args[]){
+StringBuffer a = new StringBuffer("Welocome");
+a=a.append(" to");
+a=a.append("Bhopal");
+System.out.println(a);
+}
+}
---
+++ String/trimm.java
@@ -0,0 +1,21 @@
+//trim method() this is use for remove first and //lastspace
+
+class trimm{
+
+public static void main(String args[]){
+
+/*String str=" Hello world  ";
+System.out.println("Length before trim:"+str.length());
+String s=str.trim();
+System.out.println("Length of after trim:"+s.length());
+}
+}
+*/
+
+//valueOf method() in java
+int a=1234;
+String x=String.valueOf(a);
+System.out.println(x.length());
+}
+}
+
