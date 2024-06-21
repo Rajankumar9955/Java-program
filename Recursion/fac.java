60b0fa48e1b927458433b1c752a08d047e96237a
Rajan <krajan92946@gmail.com>
2024-06-21 18:36:47 +0530

    recursion

---
+++ Recursion/fac.java
@@ -0,0 +1,19 @@
+//recursion
+
+class fac{
+public static int  fact(int n){
+      if(n==1)
+    {
+	return 1;
+    }
+    else 
+	{
+	   return n*(fact-1);
+	}
+
+{
+public static void main(String args[]){
+ Sysetem.out.println("factorial:"+fact(5));
+}
+}
+ 
