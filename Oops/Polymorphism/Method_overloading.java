11b3b0add4c687f09bf7216b62e28f838ac61e7d
Rajan <krajan92946@gmail.com>
2024-07-04 18:34:54 +0530

    oops

---
+++ Oops/Polymorphism/Method_overloading.java
@@ -0,0 +1,49 @@
+ class Method_overloading {
+    public void add(){
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println("Addition without argument:"+c);
+    }
+
+
+
+    public int add(int a,int b)
+    {
+        int c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println("Addition with int argument+"+c);
+        return c;
+    }
+
+
+
+    public void add(float a, float b)
+    {
+        float c=a+b;
+        System.out.println("add two float"+c);
+    }
+
+
+
+    public  void add(double a, double b)
+    {
+        double c=a+b;
+        System.out.println("add two double"+c);
+    }
+
+
+
+    public void add(String a, String b){
+       int  c= Integer.parseInt(a)+Integer.parseInt(b);
+        System.out.println("add of String "+c);
+    }
+}
+
+public static void main(String args[])
+{
+   
+}
