1731113dd67e38d413c2c93dfb4c6ec441bb9963
Rajan <krajan92946@gmail.com>
2024-09-15 15:40:57 +0530

    company view questions

---
+++ Company view Questions/reverseString.java
@@ -0,0 +1,11 @@
+
+
+public class reverseString {
+    public static void main(String[] args)
+    {
+        String str="Rajan";
+        StringBuilder reversed=new StringBuilder(str).reverse();
+        System.out.println(reversed);
+    }
+    
+}
