9d868d794fef72d4711b2eb7ab3000223fb9bcc3
Rajan <krajan92946@gmail.com>
2024-09-16 16:03:22 +0530

    sort array

---
+++ Company view Questions/SortArray.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,86,8,68,9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
