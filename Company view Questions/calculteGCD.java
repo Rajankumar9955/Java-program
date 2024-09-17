f427f7390b301d3863ac21d6caa053e87d77899a
Rajan <krajan92946@gmail.com>
2024-09-17 18:51:21 +0530

    calculate GCD

---
+++ Company view Questions/calculteGCD.java
@@ -0,0 +1,13 @@
+public class calculteGCD {
+    public static void main(String[] args) {
+        int a=60,b=48;
+        while(b!=0)
+        {
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        System.out.println(a);
+    }
+    
+}
