80efff4bce74fdf0ad8ab41c3b1bbb3cc37c8994
Rajan <krajan92946@gmail.com>
2024-09-19 22:39:38 +0530

    Duplicate number

---
+++ Company view Questions/Duplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class Duplicate {
+    public static void main(String[] args) {
+        int arr[]={1,2,2,3,4,4};
+        HashSet<Integer>set=new HashSet<>();
+        for( int num:arr)
+        {
+            set.add(num);
+        }
+        System.out.println(set);
+    }
+}
