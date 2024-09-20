9687c1b1245587a4af67048bfa4a4c25c7771723
Rajan <krajan92946@gmail.com>
2024-09-20 23:51:58 +0530

    dectoBinary

---
+++ Company view Questions/DecToBinary.java
@@ -0,0 +1,7 @@
+public class DecToBinary {
+    public static void main(String[] args) {
+        int num=10;
+        String binary=Integer.toBinaryString(num);
+        System.out.println(binary);
+    }
+}
