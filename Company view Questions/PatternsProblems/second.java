e0c1f9ef47a1dc22643b50473ddaf1f2ab3a0f63
Rajan <krajan92946@gmail.com>
2024-09-21 19:17:01 +0530

    secondquestions

--- Company view Questions/PatternsProblems/second.java
+++ Company view Questions/PatternsProblems/second.java
@@ -1,11 +1,10 @@
 
 package PatternsProblems;
 
-
-public class first {
+public class second {
     public static void main(String[] args) {
         int n=5;
-        for(int i=1; i<=n; ++i)
+        for(int i=n; i>=1; --i)
         {
             for(int j=1; j<=i; ++j)
             {
