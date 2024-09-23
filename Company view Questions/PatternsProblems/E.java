af98931095510e308f791497eb42126ab028c125
Rajan <krajan92946@gmail.com>
2024-09-23 16:57:37 +0530

    E Pattern

---
+++ Company view Questions/PatternsProblems/E.java
@@ -0,0 +1,19 @@
+public class E {
+    public static void main(String[] args) {
+        for(int i=1; i<=7; i++)
+        {
+            for(int j=1; j<=7; j++)
+            {
+                if(j==1||i==1||i==4||i==7)
+                {
+                    System.out.print("*");
+                }else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
