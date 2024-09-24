9a964eca6a65dc152baee63d3ce240b977967375
Rajan <krajan92946@gmail.com>
2024-09-24 17:23:20 +0530

    K pattern

---
+++ Company view Questions/PatternsProblems/K.java
@@ -0,0 +1,19 @@
+public class K {
+    public static void main(String[] args) {
+        for(int i=1; i<=7; i++)
+        {
+            for(int j=1; j<=7; j++)
+            {
+                if(j==1||(j==6-i)||(i==2+j))
+                {
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
