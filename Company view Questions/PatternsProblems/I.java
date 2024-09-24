e78a3302c66a1db2be92a8558b6f645436dd189e
Rajan <krajan92946@gmail.com>
2024-09-24 17:17:32 +0530

    I pattern

---
+++ Company view Questions/PatternsProblems/I.java
@@ -0,0 +1,19 @@
+public class I {
+    public static void main(String[] args) {
+        for(int i=1; i<=7; i++)
+        {
+            for(int j=1; j<=7; j++)
+            {
+                if(i==1||j==4||i==7)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
