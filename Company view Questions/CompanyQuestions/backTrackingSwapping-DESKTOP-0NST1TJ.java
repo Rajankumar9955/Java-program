4e36dd94ef7b9535404a94d40b236c8bcc3843fe
Rajan <krajan92946@gmail.com>
2024-09-26 11:49:56 +0530

    Swapping the String using BackTracking

---
+++ Company view Questions/CompanyQuestions/backTrackingSwapping-DESKTOP-0NST1TJ.java
@@ -0,0 +1,13 @@
+public class backTrackingSwapping {
+    public static String SwappingString(String a,int i,int j)
+    {
+        char[] b=a.toCharArray();
+        char ch;
+        ch=b[i];
+        b[i]=b[j];
+        b[j]=ch;
+        return String.valueOf(b);
+
+    }
+    public static 
+}
