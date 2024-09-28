cf7f8313f67d633344d8328d5b22289b633a3efd
Rajan <krajan92946@gmail.com>
2024-09-28 19:57:47 +0530

    nth term

---
+++ Company view Questions/SomeQuestion/fourth.java
@@ -0,0 +1,12 @@
+
+public class fourth {
+    public static int NThterm(int n)
+    {
+        return 7*(int)Math.pow(n, 2)-7*n+7;
+
+    }
+    public static void main(String[] args) {
+        int N=4;
+        System.out.println(NThterm(N));
+    }
+}
