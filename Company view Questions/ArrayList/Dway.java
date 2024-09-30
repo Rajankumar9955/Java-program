917cb2f63446dcdc0a98347173049ffc1657344a
Rajan <krajan92946@gmail.com>
2024-09-30 19:41:23 +0530

    dWays array

---
+++ Company view Questions/ArrayList/Dway.java
@@ -0,0 +1,15 @@
+
+// java program showing declaration of arrays
+import java.io.*;
+public class Dway {
+    public static void main(String[] args)
+    {
+
+        int[][] integer2DArray; // 2D integer array
+        String[][] string2DArray; // 2D String array
+        double[][] double2DArray; // 2D double array
+        boolean[][] boolean2DArray; // 2D boolean array
+        float[][] float2DArray; // 2D float array
+        double[][] double2DArray; // 2D double array
+    }
+}
