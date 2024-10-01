88738caa06659604eff6a91394cb1e06da222df6
Rajan <krajan92946@gmail.com>
2024-10-01 12:55:02 +0530

    crete4

---
+++ Company view Questions/ArrayList/create4.java
@@ -0,0 +1,12 @@
+// Java Program to implement
+// System.out
+import java.io.*;
+// Driver Class
+class create4{
+    // Main Function
+    public static void main(String[] args)
+    {
+        // Use of System.out
+        System.out.println("");
+    }
+}
