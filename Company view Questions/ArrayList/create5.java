9304a34c52d5fa52306f3dadeb2d0812c538ee63
Rajan <krajan92946@gmail.com>
2024-10-01 12:56:13 +0530

    create5

---
+++ Company view Questions/ArrayList/create5.java
@@ -0,0 +1,13 @@
+// Java program to demonstrate
+// System.err
+import java.io.*;
+// Driver Class
+class create5 {
+    // Main function
+    public static void main(String[] args)
+    {
+        // Printing error
+        System.err.println(
+            "This is how we throw error with System.err");
+    }
+}
