8f59b9bad9d54f9cbd024c3bcb88192b0a8ee363
Rajan <krajan92946@gmail.com>
2024-10-02 22:44:35 +0530

    anagram

--- Company view Questions/Anagram.java
+++ Company view Questions/Anagram.java
@@ -1,10 +1,25 @@
+// import java.util.Arrays;
+
+// public class Anagram {
+//     public static void main(String[] args) {
+//         String str1="listen",str2="silent";
+//         char[] arr1=str1.toCharArray();
+//         char[] arr2=str2.toCharArray();
+//         Arrays.sort(arr1);
+//         Arrays.sort(arr2);
+//         System.out.println(Arrays.equals(arr1, arr2));
+//     }
+// }
+
 import java.util.Arrays;
 
-public class Anagram {
-    public static void main(String[] args) {
+public class Anagram{
+    public static void main(String[] args)
+    {
         String str1="listen",str2="silent";
         char[] arr1=str1.toCharArray();
         char[] arr2=str2.toCharArray();
+
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         System.out.println(Arrays.equals(arr1, arr2));
