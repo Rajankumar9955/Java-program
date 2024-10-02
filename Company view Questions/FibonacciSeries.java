c7bb07fa485574bc83f22e80649de0578a2055d4
Rajan <krajan92946@gmail.com>
2024-10-02 22:49:48 +0530

    factorial

--- Company view Questions/FibonacciSeries.java
+++ Company view Questions/FibonacciSeries.java
@@ -1,14 +1,17 @@
 
-public class FibonacciSeries {
-    public static void main(String[] args) {
-        int n=10,num1=0,num2=1;
-        System.out.print("Fibonacci series: "+ num1+" "+num2);
-        for(int i=2; i<n; ++i)
-        {
-            int num3=num1+num2;
-            System.out.print(" "+num3);
-            num1=num2;
-            num2=num3;
-        }
-    }
-}
+// public class FibonacciSeries {
+//     public static void main(String[] args) {
+//         int n=10,num1=0,num2=1;
+//         System.out.print("Fibonacci series: "+ num1+" "+num2);
+//         for(int i=2; i<n; ++i)
+//         {
+//             int num3=num1+num2;
+//             System.out.print(" "+num3);
+//             num1=num2;
+//             num2=num3;
+//         }
+//     }
+// }
+
+
+public
--- Company view Questions/factorial.java
+++ Company view Questions/factorial.java
@@ -1,12 +1,24 @@
 
-public class factorial {
-    public static void main(String[] args) {
-        int num=5, factorial=1;
-        for(int i=1; i<=num; ++i)
+// public class factorial {
+//     public static void main(String[] args) {
+//         int num=5, factorial=1;
+//         for(int i=1; i<=num; ++i)
+//         {
+//             factorial*=i;
+//         }
+//         System.out.println(factorial);
+
+//     }
+// }
+
+
+public class factorial{
+    public static void main(String[] args){
+        int num=5,factorial=1;
+        for(int i=1; i<=num; i++)
         {
             factorial*=i;
         }
         System.out.println(factorial);
-
     }
 }
