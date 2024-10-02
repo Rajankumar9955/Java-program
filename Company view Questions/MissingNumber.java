8eab2209c29a2ab2805981d058aca91b1d0e0690
Rajan <krajan92946@gmail.com>
2024-10-02 23:11:32 +0530

    missing number

--- Company view Questions/MissingNumber.java
+++ Company view Questions/MissingNumber.java
@@ -1,13 +1,29 @@
-public class MissingNumber {
-    public static void main(String[] args) {
-       int[] arr={1,3,4,5,6};
-       int n=arr.length;
-       int sum=n*(n+1)/2;
-       int s=0;
-       for(int i=0; i<arr.length-1; ++i)
-       {
-             s+=arr[i];
-       }
-       System.out.println(sum-s);
+// public class MissingNumber {
+//     public static void main(String[] args) {
+//        int[] arr={1,3,4,5,6};
+//        int n=arr.length;
+//        int sum=n*(n+1)/2;
+//        int s=0;
+//        for(int i=0; i<arr.length-1; ++i)
+//        {
+//              s+=arr[i];
+//        }
+//        System.out.println(sum-s);
+//     }
+// }
+
+
+public class MissingNumber{
+    public static void main(String[] args)
+    {
+        int[] arr={1,3,4,5,6};
+        int n=arr.length;
+        int sum=n*(n+1)/2;
+        int s=0;
+        for(int i=0; i<arr.length-1; ++i)
+        {
+            s+=arr[i];
+        }
+        System.out.println(sum-s);
     }
 }
