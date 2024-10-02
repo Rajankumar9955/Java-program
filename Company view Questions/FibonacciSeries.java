e745bfe76e6d9f65d3494162d272f13c53dc6779
Rajan <krajan92946@gmail.com>
2024-10-02 22:55:35 +0530

    fibonacci series

--- Company view Questions/FibonacciSeries.java
+++ Company view Questions/FibonacciSeries.java
@@ -14,4 +14,17 @@
 // }
 
 
-public
+public class FibonacciSeries{
+    public static void main(String[] args)
+    {
+        int n=10; int num1=0; int num2=1;
+        System.out.print("FibonacciSeries : "+ num1 +" "+ num2);
+        for(int i=2; i<n; i++)
+        {
+            int num3=num1+num2;
+            System.out.print(" "+ num3);
+            num1=num2;
+            num2=num3;
+        }
+    }
+}
