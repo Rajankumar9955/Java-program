b8bbda112c375704d79d58078071d306417d2c48
Rajan <krajan92946@gmail.com>
2024-10-02 23:04:49 +0530

    vowel ans consonants

--- Company view Questions/CountVowelConsonants.java
+++ Company view Questions/CountVowelConsonants.java
@@ -1,19 +1,39 @@
 
-public class CountVowelConsonants {
-    public static void main(String[] args) {
+// public class CountVowelConsonants {
+//     public static void main(String[] args) {
+//         String str="Rajan";
+//         int vowels=0, consonants=0;
+//         for(char c:str.toCharArray())
+//         {
+//             if("aeiouAEIOU".indexOf(c)!=-1)
+//             {
+//                 vowels++;
+//             }
+//             else if(Character.isLetter(c))
+//             {
+//                 consonants++;
+//             }
+//         }
+//         System.out.println("Vowels: "+vowels+", consonants: "+consonants);
+//     }
+// }
+
+
+public class CountVowelConsonants{
+    public static void main(String[] args)
+    {
         String str="Rajan";
-        int vowels=0, consonants=0;
+        int vowel =0, consonants=0;
         for(char c:str.toCharArray())
         {
             if("aeiouAEIOU".indexOf(c)!=-1)
             {
-                vowels++;
+                vowel++;
             }
-            else if(Character.isLetter(c))
-            {
+            else if(Character.isLetter(c)){
                 consonants++;
             }
         }
-        System.out.println("Vowels: "+vowels+", consonants: "+consonants);
+        System.out.println("vowel: "+vowel + " consonants: "+ consonants) ;
     }
 }
