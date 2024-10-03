b48bfa6b0288450e3c0d761ad3999d6ee4344ba4
Rajan <krajan92946@gmail.com>
2024-10-03 22:34:46 +0530

    missing number in array

---
+++ COMPANYVIEW/MissingNumber.java
@@ -0,0 +1,16 @@
+
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,6,7};
+        int n=arr.length;
+        int sum=n*(n+1)/2;
+        int s=0;
+       for(int i=0; i<arr.length-1; i++)
+       {
+        s+=arr[i];
+       }
+       System.out.println(sum-s);
+    }
+    
+}
