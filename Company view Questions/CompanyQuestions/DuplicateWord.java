965d630cef871b2b92993194b91cf9bc90764bca
Rajan <krajan92946@gmail.com>
2024-10-06 18:56:43 +0530

    DuplicateWord

--- Company view Questions/CompanyQuestions/DuplicateWord.java
+++ Company view Questions/CompanyQuestions/DuplicateWord.java
@@ -13,6 +13,7 @@ public class DuplicateWord {
         for(int i=0; i<words.length; i++)
         {
             count=1;
+            
             for(int j=i+1; j<words.length; ++j)
             {
                 if(words[i].equals(words[j]))
