4bf44b27306ff41eaa3a98300213b3391615be64
Rajan <krajan92946@gmail.com>
2024-11-03 22:21:02 +0530

    nalson Number

--- Number based task1/Q7.java
+++ Number based task1/Q7.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
+
 public class Q7 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Entre any number");
+        System.out.println("Enter any number");
         int num=sc.nextInt();
         if(num%111==0)
         {
