2caa1ddb5f05365aea52dbcf4190386ef266c836
Rajan <krajan92946@gmail.com>
2024-11-04 21:04:24 +0530

    Nth term to print

--- Company view Questions/SomeQuestion/fourth.java
+++ Company view Questions/SomeQuestion/fourth.java
@@ -3,7 +3,6 @@ public class fourth {
     public static int NThterm(int n)
     {
         return 7*(int)Math.pow(n, 2)-7*n+7;
-
     }
     public static void main(String[] args) {
         int N=4;
