641fa96848abad2fa286771b1dfbece20f56d029
Rajan <krajan92946@gmail.com>
2024-11-29 23:09:48 +0530

    solve the position

--- String/String based Task/Task1/ques1.java
+++ String/String based Task/Task1/ques1.java
@@ -35,34 +35,17 @@
 /**
  * ques1
  */
-
-public class ques1 {
-
-    public static void main(String[] args) {
-        String s=new String("Betty got a bit of bitter better butter");
+public class ques1{
+    public static void main(String args[])
+    {
+        String s= new String("Betty got a bit of bitter better butter");
         String c[]=s.split(" ");
         for(int i=0; i<c.length; i++)
         {
-            System.out.println(c[i]+":"+c[i].length());
+             System.out.println(c[i]+ ":"+c[i].length());
         }
-        char ch[]=s.toCharArray();
-        for(int i=0; i<ch.length; ++i)
-        {
-            if(ch[i]=='b')
-            {
-                System.out.println("Index of b:"+i);
-            }
-            else if(ch[i]=='t')
-            {
-                System.out.println("Index of t:"+i);
-            }
-        }
-        for(int i=0; i<c.length; ++i)
-        {
-            if(c[i].charAt(0)=='b')
-            {
-                System.out.println(c[i]);
-            }
-        }
+       
+
+
     }
 }
