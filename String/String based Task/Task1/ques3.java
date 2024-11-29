bc4aaa43afaf7e24aa2153dbe51127abe83be423
Rajan <krajan92946@gmail.com>
2024-11-29 23:19:07 +0530

    string ques3 done

--- String/String based Task/Task1/ques3.java
+++ String/String based Task/Task1/ques3.java
@@ -1,4 +1,38 @@
 
+// import java.util.Scanner;
+// public class ques3 {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         String str=sc.nextLine();
+//         char c[]=str.toCharArray();
+//         if(str.length()==5)
+//         {
+//             System.out.println("String is:");
+//         }
+//         else
+//         {
+//             System.out.println("String should be in 5 character");
+//         }
+//         for(int i=0; i<c.length; ++i)
+//         {
+//             if(c[i]>='1' && c[i]<='9')
+//             {
+//                 System.out.println(c[i]);
+//             }
+//             else
+//             {
+//                 System.out.println("The charater should be in digit");
+//                 break;
+//             }
+//         }
+//     }
+    
+// }
+
+
+//-----------------------------------------------------------
+
+
 import java.util.Scanner;
 public class ques3 {
     public static void main(String[] args) {
