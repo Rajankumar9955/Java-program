809b31979c6dd242cd7fbccbce3f93efdf435b03
Rajan <krajan92946@gmail.com>
2024-12-01 23:37:24 +0530

    dayfromthedate

--- loops/Dayfromdate/Dayfromdate.java
+++ loops/Dayfromdate/Dayfromdate.java
@@ -1,4 +1,4 @@
-public class Dayfromdate {
+import java.util.Scanner;
 
 public class DayFromDate {
     public static String dayFromDate(int date,int month,int year){
@@ -34,8 +34,20 @@ public class DayFromDate {
         }
         return s;
     }
+    public static void main(String[] args) {
+        int date,month,year;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the date: ");
+        date = sc.nextInt();
+        System.out.println("Enter the month: ");
+        month = sc.nextInt();
+        System.out.println("Enter the year: ");
+        year = sc.nextInt();
+        String result = DayFromDate.dayFromDate(date,month,year);
+        System.out.println(result);
+    
 }
 
-
-
 }
+
+    
